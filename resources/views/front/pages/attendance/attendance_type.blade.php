49b5941c11fe6508753cbf46e32d9f5ebd57a802
mizan2832 <programmer620@gmail.com>
2022-08-01 18:13:37 +0600

    attendance type change

--- resources/views/front/pages/attendance/attendance_type.blade.php
+++ resources/views/front/pages/attendance/attendance_type.blade.php
@@ -21,14 +21,10 @@
 @section('content')
     <div class="attnType">
         <h1 class="text-center attnHeader">Attendance Type</h1>
-        <div class="container mt-5">
-        
-               
-                
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addType">
+        <div class="container mt-5"> 
+            <button type="button" id="add" class="btn btn-primary" data-toggle="modal" data-target="#addType">
                Add Type
-              </button>
-              
+              </button> 
                     <div class="attnTable">
                         <table class="table table-bordered table-striped text-center" id="tTable">
                             <thead>
@@ -42,7 +38,7 @@
                                     @foreach ($types as $type)
                                      <tr>
                                         <td>{{$type->name}}</td>
-                                        <td><a href="javascript:void(0)"  id="edit_type" data-id="{{ $type->id }}"><i class="far fa-edit"></i></a> <a href=""><i class="fas fa-trash-alt"></i></a>  </td>
+                                        <td><a href="javascript:void(0)"  class="edit_type" data-id="{{ $type->id }}"><i class="far fa-edit"></i></a> <a href=""><i class="fas fa-trash-alt"></i></a>  </td>
                                     </tr>
                                     @endforeach
                             </tbody>
@@ -62,14 +58,14 @@
                
                 <div class="modal-body">
                     <div class="input-group mb-3">
-                        <input type="text" id="name" class="form-control" placeholder="type name" >
-                 
+                        <input type="text" id="name" class="form-control" placeholder="type name" >         
                       </div>
                 </div>
 
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="button" id="typeSave" class="btn btn-primary">Save</button>
+                    <button type="button" id="update" class="btn btn-primary">Update</button>
                   </div>
 
               </div>
@@ -81,21 +77,23 @@
 @endsection
 @push('js')
     <script>
+      $('#add').click(function(){
+        $('#update').hide();
+        $('#typeSave').show();
+      })
 
       $('#typeSave').click(function(){
         var name = $('#name').val();
-        console.log(name);
         $.ajax({
             type:'POST',
             dataType:'json',
             url:'/save',
             data:{"_token": $('#token').val(),name:name},
             success: function(data){
-                console.log(data.id);
                 var name = data.name;
                 var data = "<tr>";
                     data += "<td id='"+data.id+"'>"+name+"</td>";
-                    data += "<td><a href='#' id='edit_type' data-id='"+data.id+"'><i class='far fa-edit'></i></a> <a href=''><i class='fas fa-trash-alt'></i></a> </td>";
+                    data += "<td><a href='#' class='edit_type' data-id='"+data.id+"'><i class='far fa-edit'></i></a> <a href=''><i class='fas fa-trash-alt'></i></a> </td>";
                     data += "</tr>";
                 let tableBody = $('#tTable tbody').append(data);
                
@@ -104,20 +102,20 @@
         })
       });
 
-      $('#edit_type').click(function(){
-        var type_id = $(this).data("id");
-        alert('agag');
-        $.ajax({
-            method:'GET',
-            dataType:'JSON',
-            data : {data:type_id},
-            url : '/attendance-type/edit/'+type_id,
-            success:function(data){
-
-                console.log(data);
+      $('.edit_type').click(function(){
+        var type_id = $(this).data('id');
+        console.log(type_id);
+            $.ajax({
+                method:'GET',
+                dataType:'JSON',
+                data : {data:type_id},
+                url : '/attendance-type/edit/'+type_id,
+                success:function(data){
+                    $('#addType').modal('show');
+                    $('#name').val(data.name);  
+                    $('#typeSave').hide();
+                    $('#update').show();
             }
-
-
         })
 
 
