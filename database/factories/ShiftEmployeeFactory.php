b9801edbe3e02497ea2b201d13a0f47ab6c84c46
mizan2832 <programmer620@gmail.com>
2022-08-04 23:53:46 +0600

    Shit wise employes

---
+++ database/factories/ShiftEmployeeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ShiftEmployee;
+use Faker\Generator as Faker;
+
+$factory->define(ShiftEmployee::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
