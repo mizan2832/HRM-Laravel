fd028cbee0f8608d851a93229b8bd16888588a12
mizan2832 <programmer620@gmail.com>
2022-10-11 07:44:30 +0600

    complete employee search

--- resources/views/front/pages/employee/employee_list.blade.php
+++ resources/views/front/pages/employee/employee_list.blade.php
@@ -3,6 +3,29 @@
 @push('head')
 @endpush
 @section('content')
+<h2 style="text-align: center">Employee List</h2>
+
+<form class="form-inline">
+
+  <label for="name" class="mr-sm-2">Name</label>
+  <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Enter name" id="name">
+
+  <label for="email" class="mr-sm-2">Email</label>
+  <input type="email" class="form-control mb-2 mr-sm-2" placeholder="Enter email" id="email">
+
+  <label for="number" class="mr-sm-2">Mobile</label>
+  <input type="number" class="form-control mb-2 mr-sm-2" placeholder="Enter mobile" id="mobile">
+{{--       
+  <label for="select" class="mr-sm-2">Select</label>
+  <select name="select" id="select" class="form-control mb-2 mr-sm-2">
+    <option value="">Select option</option>
+    <option value="department">Department</option>
+    <option value="joining_date">Joining Date</option>
+    <option value="section">Section</option>
+    <option value="unit">Unit</option>
+  </select> --}}
+
+</form>
 <div id="employee_data">
   @include('front.pages.employee.employee_table')
 </div>
